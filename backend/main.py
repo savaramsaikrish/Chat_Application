c20f63b6e5a4542a78ff7df9f79908246182aa14
SAVARAM SAI KRISHNA CHAITANYA PRABHU <143736535+savaramsaikrish@users.noreply.github.com>
2024-04-10 08:38:44 +0530

    main.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
 import requests
-PRIVATE_KEY = "f200ccee-5f13-4ff4-98a0-ea247a667003"
+PRIVATE_KEY = "......"
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
